7f0e9bc8b15db84c9ccf4e4aa473771b034e8da5
ezeanello <ezeanello96@gmail.com>
2015-09-10 02:23:18 -0300

    Correccion de funciones de la pagina de registro

--- www/js/funciones.js
+++ www/js/funciones.js
@@ -26,7 +26,7 @@ function crearDB(){
 //La funcion que crea la base de datos y todas sus tablas
 
 function populateDB(tx) {
-    tx.executeSql('create table if not exists PERSONA(usuario VARCHAR(255) NOT NULL, nombre VARCHAR(255), edad INTEGER, contrasenia VARCHAR(255), sexo VARCHAR(255), PRIMARY KEY (usuario))');
+    tx.executeSql('create table if not exists PERSONA(usuario VARCHAR(255) NOT NULL, numero INTEGER NOT NULL, nombre VARCHAR(255), edad INTEGER, contrasenia VARCHAR(255), sexo VARCHAR(255), PRIMARY KEY (usuario))');
     tx.executeSql('create table if not exists clubf(id INTEGER NOT NULL, persona1 VARCHAR(255), persona2 VARCHAR(255), PRIMARY KEY (id))');
     tx.executeSql('create table if not exists gala(id INTEGER NOT NULL, persona1 VARCHAR(255), persona2 VARCHAR(255), PRIMARY KEY (id))');
     tx.executeSql('create table if not exists rosario(id INTEGER NOT NULL, persona1 VARCHAR(255), persona2 VARCHAR(255), PRIMARY KEY (id))');
@@ -37,10 +37,11 @@ function populateDB(tx) {
 
 //La funcion de registro
 
-function registrar(usuario, pass, nombre, edad, sexo) {
+function registrar(usuario, numero, pass, nombre, edad, sexo) {
   var db = openDatabase('JON', '1.0', 'My Sample DB', 100 * 1024);
-  db.transaction(function alguna(tx){tx.executeSql("insert into PERSONA (usuario, nombre, edad, contrasenia, sexo) values ('"+usuario+"', '"+nombre+"', '"+edad+"', '"+pass+"', '"+sexo+"')");}, successCB, errorCB);
-  window.locationf="/login.html"
+  db.transaction(function alguna(tx){tx.executeSql("insert into PERSONA (usuario, numero, nombre, edad, contrasenia, sexo) values ('"+usuario+"', '"+numero+"', '"+nombre+"', '"+edad+"', '"+pass+"', '"+sexo+"')");}, errorCB);
+  alert("Lograste registrarte pete");
+  window.locationf="/index.html";
 }
 
 //Con esta funcion se compara si las personas son compatibles o no
@@ -66,12 +67,12 @@ function entrarBoliche (usuario, boliche) {
 //Esta funcion es la que se hace para loguearse
 
 function login (usuario, contrasenia) {
-	tx.executeSql('SELECT * FROM PERSONA WHERE ', [boliche, persona1, persona2], function (tx, results) {
-  var len = results.rows.length, i;
-  for (i = 0; i < len; i++) {
-    alert(results.rows.item(i).text);
-  }
-});
+    tx.executeSql("SELECT * FROM PERSONA WHERE usuario = '"+usuario+"' AND contrasenia = '"+contrasenia+"'" , function (tx, results) {
+    var len = results.rows.length;
+    if (len == 1) {
+        window.locationf="/index.html";
+      };
+    });
 }
 
 //Esta funcion carga los distintos usuarios que van a ir al mismo boliche que vos
